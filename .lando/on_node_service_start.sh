6bc151d25d08dde1f99fc7906191c049d67f4eb8
James <james.auble@gmail.com>
2023-06-26 16:36:58 +0900

    added on node service start script

---
+++ .lando/on_node_service_start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+run_theme_watch() {
+  cd /app/wordpress/wp-content/themes/wauble/ && npm run watch &
+  PID=$!
+  echo $PID > /tmp/npm.pid
+  wait $PID
+  return 1
+}
+
+until (ps -p $(cat /tmp/npm.pid)); do
+  echo "$PID is running"
+  pkill -15 "npx mix watch"
+  pkill -15 "npm run watch"
+  pkill -15 "mix watch"  
+  pkill -15 "watch"
+  pkill -15 "npm"
+  pkill -15 "webpack"
+  run_theme_watch
+done
