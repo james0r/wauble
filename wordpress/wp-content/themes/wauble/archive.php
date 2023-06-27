3d96be6bb901c1d2737332ab47d1b5ebf21ea818
James <james.auble@gmail.com>
2023-06-27 21:09:58 +0900

    added some basic styling to the archive template

--- wordpress/wp-content/themes/wauble/archive.php
+++ wordpress/wp-content/themes/wauble/archive.php
@@ -1,26 +1,31 @@
 <?php get_template_part('template-parts/header'); ?>
 
-<h1 class="entry-title">
-  <?php echo get_the_archive_title(); ?>
-</h1>
+<div class="px-6 md:px-8 py-8">
+  <div class="container">
+    <h1 class="entry-title">
+      <?php echo get_the_archive_title(); ?>
+    </h1>
+    
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+        <?php
+        get_template_part('template-parts/content', get_post_type());
+        ?>
+        <hr>
+    <?php endwhile;
+    endif; ?>
+    
+    <h2>Archives by Month:</h2>
+    <ul>
+      <?php wp_get_archives('type=monthly'); ?>
+    </ul>
+    
+    <h2>Archives by Subject:</h2>
+    <ul>
+      <?php wp_list_categories(); ?>
+    </ul>
+  </div>
+</div>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-    <?php
-    get_template_part('template-parts/content', get_post_type());
-    ?>
-    <hr>
-<?php endwhile;
-endif; ?>
-
-<h2>Archives by Month:</h2>
-<ul>
-  <?php wp_get_archives('type=monthly'); ?>
-</ul>
-
-<h2>Archives by Subject:</h2>
-<ul>
-  <?php wp_list_categories(); ?>
-</ul>
 
 <?php get_template_part('template-parts/footer');
