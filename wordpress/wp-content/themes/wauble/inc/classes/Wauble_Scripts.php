60e9e551c326e74fca2e87e4c15738fb02441e63
James <james.auble@gmail.com>
2023-10-31 11:51:36 +0900

    whitespace

--- wordpress/wp-content/themes/wauble/inc/classes/Wauble_Scripts.php
+++ wordpress/wp-content/themes/wauble/inc/classes/Wauble_Scripts.php
@@ -87,7 +87,7 @@ class Wauble_Scripts {
     if (function_exists('wpcf7_enqueue_scripts')) {
       wpcf7_enqueue_scripts();
     }
-    
+
     if (function_exists('wpcf7_enqueue_styles')) {
       wpcf7_enqueue_styles();
     }
