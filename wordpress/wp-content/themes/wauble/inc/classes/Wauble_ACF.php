26227519be2367e914f2b9c16b27d5861293c967
James <james.auble@gmail.com>
2024-01-25 19:12:50 +0900

    removed unused field value filtering

--- wordpress/wp-content/themes/wauble/inc/classes/Wauble_ACF.php
+++ wordpress/wp-content/themes/wauble/inc/classes/Wauble_ACF.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This class initializes Advanced Custom Fields.
+ * This class contains methods for working with Advanced Custom Fields.
  */
 
 class Wauble_ACF {
@@ -13,7 +13,6 @@ class Wauble_ACF {
       return Wauble::$text_domain;
     });
     add_action('acf/init', [$this, 'acf_register_options_pages']);
-    add_filter('acf/load_field/name=post_type', [$this, 'acf_load_post_type_field_choices']);
     
     // Uncomment the following lines and create a /blocks dir in the project root to enable ACR blocks.
     // See: https://github.com/james0r/wauble/tree/e498d51c6c97f6c58b88b63b6d6ee700df9767f3/blocks
@@ -22,6 +21,12 @@ class Wauble_ACF {
     // add_filter('acf/settings/php_load', [$this, 'acf_php_load_point']);
   }
 
+  /**
+   * Sets the ACF JSON load point.
+   *
+   * @param array $paths The array of ACF JSON load paths.
+   * @return array The modified array of ACF JSON load paths.
+   */
   public function acf_json_load_point($paths) {
     unset($paths[0]);
 
@@ -30,12 +35,24 @@ class Wauble_ACF {
     return $paths;
   }
 
+  /**
+   * Sets the ACF JSON save point path.
+   *
+   * @param string $path The path to the ACF JSON save point.
+   * @return string The updated path to the ACF JSON save point.
+   */
   public function acf_json_save_point($path) {
     $path = Wauble()->path('/inc/acf-json');
 
     return $path;
   }
 
+  /**
+   * This method modifies the ACF PHP load points by adding a new path to the array of paths.
+   *
+   * @param array $paths The array of ACF PHP load points.
+   * @return array The modified array of ACF PHP load points.
+   */
   public function acf_php_load_point($paths) {
     unset($paths[0]);
 
@@ -44,45 +61,42 @@ class Wauble_ACF {
     return $paths;
   }
 
+  /**
+   * This method modifies the ACF PHP save point by setting it to the path of the ACF PHP directory.
+   *
+   * @param string $path The path to the ACF PHP save point.
+   * @return string The updated path to the ACF PHP save point.
+   */
   public function acf_php_save_point($path) {
     $path = Wauble()->path('/inc/acf-php');
 
     return $path;
   }
 
+
+  /**
+   * Registers ACF block types.
+   * 
+   * @return void
+   */
   public function acf_register_block_types() {
     if (function_exists('acf_register_block_type')) {
       Wauble()->requireOnce('/blocks/block-types.php');
     }
   }
 
+  /**
+   * Registers options pages using Advanced Custom Fields (ACF).
+   *
+   * This method checks if the ACF plugin is active and then registers the options pages
+   * by calling the `acf_add_options_page` function. The options pages are defined in the
+   * '/inc/acf-register-options-pages.php' file.
+   *
+   * @return void
+   */
   public function acf_register_options_pages() {
     if (function_exists('acf_add_options_page')) {
       Wauble()->requireOnce('/inc/acf-register-options-pages.php');
     }
-  }
-
-  public function acf_load_post_type_field_choices($field) {
-
-    // reset choices
-    $field['choices'] = array();
-
-    // get the textarea value from options page without any formatting
-    $choices = get_post_types();
-
-    // remove any unwanted white space
-    $choices = array_map('trim', $choices);
-
-    // loop through array and add to field 'choices'
-    if (is_array($choices)) {
-
-      foreach ($choices as $choice) {
-
-        $field['choices'][$choice] = $choice;
-      }
-    }
-
-    // return the field
-    return $field;
   }
 }
