48ca59615d8cfbcc2899f6264f808771755a5349
James <james.auble@gmail.com>
2024-01-25 16:13:50 +0900

    added under construction favicon

--- wordpress/wp-content/themes/wauble/template-parts/head.php
+++ wordpress/wp-content/themes/wauble/template-parts/head.php
@@ -7,6 +7,7 @@
     name="viewport"
     content="width=device-width, initial-scale=1.0"
   />
+  <link rel="icon" type="image/x-icon" href="<?php echo Wauble()->url('/static/images/favicon.ico'); ?>">
   <style>
   [x-cloak] {
     display: none !important;
