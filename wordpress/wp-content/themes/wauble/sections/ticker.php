d315463375b6dcba402e9a8976f98bdcace928f3
James <james.auble@gmail.com>
2024-07-31 14:37:02 -0700

    made ticker dynamic

--- wordpress/wp-content/themes/wauble/sections/ticker.php
+++ wordpress/wp-content/themes/wauble/sections/ticker.php
@@ -4,13 +4,12 @@ $speed = $section['speed'] ?? 500;
 $gap = $section['gap'] ?? 0;
 ?>
 
-<div
-  class="px-6 md:px-8 py-12"
->
+<div class="px-6 md:px-8 py-12">
   <div class="container text-2xl font-semibold">
     <div
-      id="marquee"
-      class="marquee overflow-hidden"
+      data-speed="<?php echo $speed; ?>"
+      data-gap="<?php echo $gap; ?>"
+      class="marquee-container overflow-hidden"
     >
       <?php echo $text; ?>
     </div>
@@ -18,13 +17,35 @@ $gap = $section['gap'] ?? 0;
 </div>
 
 <?php if ($section_is_first_instance) : ?>
+<link rel="stylesheet" href="<?php echo Wauble()->url('/static/css/vanilla-infinite-marquee.css'); ?>">
 <script type="module">
-import marquee from '<?php echo Wauble()->url('/static/js/vanilla-marquee.js'); ?>'
+import vanillaInfiniteMarquee from '<?php echo Wauble()->url('/static/js/vanilla-infinite-marquee.js'); ?>'
 
-new marquee(document.getElementById('marquee'), {
-  duplicated: true,
-  gap: <?php echo $gap; ?>,
-  speed: <?php echo $speed; ?>,
+const marqueeContainers = document.querySelectorAll('.marquee-container');
+
+marqueeContainers.forEach((c) => {
+  new vanillaInfiniteMarquee({
+    element: c,
+    speed: c.dataset.speed,
+    gap: c.dataset.gap,
+    smoothEdges: true,
+    direction: 'left',
+    spaceBetween: '25px',
+    duplicateCount: 5,
+    mobileSettings: {
+      direction: 'top',
+      speed: 2000
+    },
+    on: {
+      beforeInit: () => {
+        console.log('Not Yet Initialized');
+      },
+
+      afterInit: () => {
+        console.log('Initialized');
+      }
+    }
+  });
 });
 </script>
 <?php endif; ?>
